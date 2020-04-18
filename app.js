da8b413daa41504bb672e57ce79ae4931b0b86b9
yasaswini dasari <tcs15b010@iittp.ac.in>
2020-04-18 11:05:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
 // Define constants
+
+const selectr = document.getElementById('select');
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
@@ -43,6 +45,30 @@ function cameraStart() {
             console.error("Oops. Something is broken.", error);
         });
 }
+
+function myfun(){
+ track.stop();
+  const videoConstraints = {};
+  if (selectr.value === '') {
+    videoConstraints.facingMode = 'user';
+  } else {
+    videoConstraints.deviceId = { exact: selectr.value };
+  }
+  const constraints = {
+    video: videoConstraints,
+    audio: false
+  };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+			alert(error);
+        });
+};
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
