bfafa2d13cf503d2e672572034aea3b33db5b94e
yasaswini dasari <tcs15b010@iittp.ac.in>
2020-04-18 11:49:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,34 @@ var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
 // Define constants
+const selectr = document.getElementById('select');
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger");
+
+function cameraStart_ing() {
+        cameraStart() ;
+	    navigator.mediaDevices.enumerateDevices().then(gotDevices);
+       }
+ function gotDevices(mediaDevices) {
+		//alert(mediaDevices);
+    //  select.innerHTML = 'select camera';
+      // select.appendChild(document.createElement('option'));
+        let count = 1;
+		var c = 0 ;
+		
+       mediaDevices.forEach(mediaDevice => {
+    if (mediaDevice.kind === 'videoinput') {
+      const option = document.createElement('option');
+      option.value = mediaDevice.deviceId;
+      const label = mediaDevice.label || `Camera ${count++}`;
+      const textNode = document.createTextNode(label);
+      option.appendChild(textNode);
+      select.appendChild(option);
+    }
+  });
+      }
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
@@ -21,6 +45,31 @@ function cameraStart() {
         });
 }
 
+function myfun(){
+ track.stop();
+  const videoConstraints = {};
+  if (selectr.value === '') {
+    videoConstraints.facingMode = 'user';
+  } else {
+    videoConstraints.deviceId = { exact: selectr.value };
+  }
+  const constraints = {
+    video: videoConstraints,
+    audio: false
+  };
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+			alert(error);
+        });
+};
+
+
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
@@ -32,7 +81,7 @@ cameraTrigger.onclick = function() {
 };
 
 // Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
+window.addEventListener("load", cameraStart_ing, false);
 
 
 
