a6d11cfd531addde22dd759113c3aea68599ba11
yasaswini dasari <tcs15b010@iittp.ac.in>
2020-04-18 13:48:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,53 +31,23 @@ function gotDevices(mediaDevices) {
       }
 
 function cameraStart() {
-	document.getElementById("p1").innerHTML = "cam start fun()";
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
             track = stream.getTracks()[0];
             cameraView.srcObject = stream;
-	    document.getElementById("p1").innerHTML = "cam start";
         })
         .catch(function(error) {
             console.error("Oops. Something is broken.", error);
-	    document.getElementById("p1").innerHTML = "cam no access";
+	    alert(error);
         });
 }
 
 function cameraStart_ing() {
-	document.getElementById("p1").innerHTML = "you r in";
-	funti();
-   //     cameraStart() ;
-	//    navigator.mediaDevices.enumerateDevices().then(gotDevices);
+        cameraStart() ;
+	    navigator.mediaDevices.enumerateDevices().then(gotDevices);
        }
- function funti()
-	{
-		document.getElementById("p1").innerHTML = "funti";
-		if (!("mediaDevices" in navigator)) {
-    navigator.mediaDevices = {};
-			document.getElementById("p1").innerHTML = "no media devices";
-			
-  }
-
-  if (!("getUserMedia" in navigator.mediaDevices)) {
-	  document.getElementById("p1").innerHTML = "gey user mediap1";
-    navigator.mediaDevices.getUserMedia = constraints => {
-      const getUserMedia =
-        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-      if (!getUserMedia) {
-	      	  document.getElementById("p1").innerHTML = "gey user mediap2";
-        return Promise.reject(new Error("getUserMedia is not supported"));
-      } else {
-	      	  document.getElementById("p1").innerHTML = "gey user mediap3";
-        return new Promise((resolve, reject) =>
-          getUserMedia.call(navigator, constraints, resolve, reject)
-        );
-      }
-    };
-  }
-	}
+ 
 
 // Access the device camera and stream to cameraView
 
@@ -94,7 +64,6 @@ function myfun(){
     video: videoConstraints,
     audio: false
   };
-	
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(function(stream) {
