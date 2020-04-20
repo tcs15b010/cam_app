68ec70f109b65099cab9b439de98f60f56b31365
yasaswini dasari <tcs15b010@iittp.ac.in>
2020-04-20 10:57:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ function cameraStart() {
         })
         .catch(function(error) {
             console.error("Oops. Something is broken.", error);
-	    alert(error);
+	    alert('camera not accessible');
         });
 }
 
@@ -72,7 +72,7 @@ function myfun(){
         })
         .catch(function(error) {
             console.error("Oops. Something is broken.", error);
-			alert(error);
+			alert('camera not accessible');
         });
 };
 
